e83029a4a3cd50e634168bedb9384736ac1b44e3
Yael Brown <yaelrbrown@gmail.com>
2019-10-01 02:46:31 -0500

    Created static route to public folder.

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 Sandbox for ExpressJS with working crud operations.
 
 
+npm install -d ....
+The -d is for development
--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
+const path = require('path');
 
 // Init express
 const app = express();
 const port = process.env.PORT || 5000;
 
 // Endpoint and Route handlers
-app.get('/', (req, res) => res.send("Hello World!"));
+// app.get('/', (req, res) => res.send("Hello World!"));
+
+// Static endpoint handler
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Listen on a port
 app.listen(port, () => console.log(`Listening on port: ${port}`));
